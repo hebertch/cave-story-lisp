edbcb13ec4f2f3cf870e3a6573fad54bbbb06936
Christopher Hebert <hebert.christopherj@gmail.com>
2016-01-10 16:25:38 -0800

    add **'s to input's parameters

--- input.lisp
+++ input.lisp
@@ -1,15 +1,5 @@
 (in-package :cave-story)
 
-(defun open-joystick ()
-  "Tries to open a joystick for reading input."
-  (when (plusp (sdl:num-joysticks))
-    (let ((idx 0))
-      (let ((joystick (sdl:joystick-open 0)))
-	(when (cffi:null-pointer-p joystick)
-	  (format t "Failed to Open Joystick at index ~A. ~A~%" idx (sdl:get-error))
-	  (nilf joystick))
-	joystick))))
-
 (defstruct (transient-input
 	     (:conc-name ti-))
   "Input that occurs in a single frame"
@@ -34,13 +24,26 @@
   joy-axis-y
   (transient-input (make-transient-input)))
 
+(defvar *event*)
+(defvar *joystick*)
+(defparameter *show-joy-buttons?* nil)
+
 (defun reset-transient-input (input)
+  "Return a copy of input with the transient-input cleared."
   (let ((input (copy-structure input)))
     (setf (input-transient-input input) (make-transient-input))
     input))
 
-(defvar *event*)
-(defvar *joystick*)
+(defun open-joystick ()
+  "Tries to open a joystick for reading input."
+  (when (plusp (sdl:num-joysticks))
+    (let ((idx 0))
+      (let ((joystick (sdl:joystick-open 0)))
+	(when (cffi:null-pointer-p joystick)
+	  (format t "Failed to Open Joystick at index ~A. ~A~%" idx (sdl:get-error))
+	  (nilf joystick))
+	joystick))))
+
 (defun init-input ()
   (setf *event* (sdl:create-event)
 	*joystick* (open-joystick)))
@@ -52,8 +55,6 @@
     (when (sdl:joystick-get-attached *joystick*)
       (sdl:joystick-close *joystick*))
     (nilf *joystick*)))
-
-(defparameter show-joy-buttons? nil)
 
 (defun gather-transient-input ()
   "Gather all input for a frame into a transient-input object."
@@ -87,7 +88,7 @@
 	    (when (= 0 (sdl:event-get-joystick-which *event*))
 	      (let ((button (sdl:event-get-joystick-button *event*)))
 		(pushnew button (ti-pressed-joy-buttons ti))
-		(when show-joy-buttons?
+		(when *show-joy-buttons?*
 		  (format t "Button Pressed: ~A~%" button)))))
 	   (:joy-button-up
 	    (when (= 0 (sdl:event-get-joystick-which *event*))
