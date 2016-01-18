952911570c0e4bff800d59ac16ca0c007d8884ff
Christopher Hebert <hebert.christopherj@gmail.com>
2016-01-17 21:38:13 -0800

    camera is an alist

--- camera.lisp
+++ camera.lisp
@@ -16,22 +16,23 @@
 	 (zero-v))
      (offset-in-dir-pos pos (tiles 3) h-facing))))
 
-(defstruct (camera (:include entity-state)) player)
+(defun camera-fns-alist ()
+  (alist :ai-fn #'camera-ai))
 
 (defun make-default-camera (focus vel player)
-  (make-camera :physics
-	       (alist :target
-		      (make-target-kin-2d :pos focus
-					  :vel vel
-					  :target
-					  (camera-target-from-player
-					   (estate
-					    player))
-					  :target-vel
-					  (player-vel
-					   (estate
-					    player))))
-	       :player player))
+  (amerge
+   (camera-fns-alist)
+   (alist
+    :physics
+    (alist
+     :target
+     (make-target-kin-2d
+      :pos focus
+      :vel vel
+      :target (camera-target-from-player (estate player))
+      :target-vel (player-vel (estate player))))
+    :player player)))
+
 (def-entity-constructor create-player-camera #'make-default-camera
   :timers :physics)
 
@@ -42,51 +43,48 @@
 
 (defun add-camera-shake ()
   (lambda (physics)
-    (aset (aset physics :shake-v (make-shake))
+    (aset physics
+	  :shake-v (make-shake)
 	  :shake-h (make-shake))))
 
 (defun camera-ai (c ticks)
   (let ((physics (aupdate
-		  (camera-physics c)
+		  (aval c :physics)
 		  (lambda (m)
 		    (target-kin-2d-update-target
 		     m
-		     (camera-target-from-player (estate (camera-player c)))
-		     (player-vel (estate (camera-player c)))))
+		     (camera-target-from-player (estate (aval c :player)))
+		     (player-vel (estate (aval c :player)))))
 		  :target))
 	(shake-tick? (member :shake ticks)))
 
-    (make-camera
-     :physics (if shake-tick?
-		  (arem physics :shake-h :shake-v)
-		  physics)
-     :timers (if shake-tick?
-		 (arem (camera-timers c) :shake)
-		 (camera-timers c))
-     :player (camera-player c))))
-
-(defmethod ai ((c camera) ticks)
-  (camera-ai c ticks))
+    (aset c
+	  :physics (if shake-tick?
+		       (arem physics :shake-h :shake-v)
+		       physics)
+	  :timers (if shake-tick?
+		      (arem (aval c :timers) :shake)
+		      (aval c :timers)))))
 
 (defun timed-camera-shake (c time)
-  (make-camera :physics (funcall (add-camera-shake)
-				 (camera-physics c))
-	       :timers (aset (camera-timers c)
-			     :shake
-			     (create-expiring-timer time t))
-	       :player (camera-player c)))
+  (aset c
+	:physics (funcall (add-camera-shake)
+			  (aval c :physics))
+	:timers (aset (aval c :timers)
+		      :shake
+		      (create-expiring-timer time t))))
 
 (defun stage-dims->camera-bounds (stage-dims)
   (create-rect (scale-v *window-dims* 1/2)
 	       (sub-v stage-dims *window-dims*)))
 
 (defun camera-focus (c)
-  (target-kin-2d-pos (aval (camera-physics c) :target)))
+  (target-kin-2d-pos (aval (aval c :physics) :target)))
 
 (defun camera-pos (camera camera-bounds)
   (let ((pos (clamp-pos (camera-focus camera) camera-bounds))
-	(shake-h (aval (camera-physics camera) :shake-h))
-	(shake-v (aval (camera-physics camera) :shake-v)))
+	(shake-h (aval (aval camera :physics) :shake-h))
+	(shake-v (aval (aval camera :physics) :shake-v)))
     (+v pos
 	(if shake-h (wave-offset shake-h) (zero-v))
 	(if shake-v (wave-offset shake-v) (zero-v))
