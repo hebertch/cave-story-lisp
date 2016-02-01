d49031f4de60bc8d86337695e2b90d48fc4805ca
Christopher Hebert <hebert.christopherj@gmail.com>
2016-01-31 18:05:08 -0800

    added a lot of pixtone stuff. wip

--- cave-story.lisp
+++ cave-story.lisp
@@ -3162,3 +3162,264 @@ The number of smoke particles to create when destroyed.")
 				      (:down 3)))
 				 25/2))
     :pos (tile-pos (aval s :tile-pos)))))
+
+(defparameter *pxt-wave-models*
+  '((0 :sine)
+    (1 :triangle)
+    (2 :saw-up)
+    (3 :saw-down)
+    (4 :square)
+    (5 :random))
+  "List of (index . wave-model).")
+
+(defparameter *pxt-channel-fields* 
+  '((:use :bool "Whether the channel/track should be used.")
+    (:size :int "Size in bytes of the sound sample. All frequencies are
+relative to the size (not 1 second!), where 1 second of sound is 22050 bytes.
+So at size=22050 440 Hz is A4, but at size=11025 440Hz is A5, since it is now
+oscillating twice as fast.")
+    (:main-model :wave-model "Wave model to use. Sin/triangle/etc.")
+    (:main-freq :float "Frequency. 440 Hz is A4.")
+    (:main-top :int "Amplitude of wave on a scale of 0 to 63. At 63, with no modulation,
+the volume is 1/2 a decibel.")
+    (:main-offset :int "The x offset of the wave.")
+    (:pitch-model :wave-model "Wave model to use for the pitch modulation.
+Formula for pitch is different depending on whether the pitch wave is above
+or below 0.
+Let f' be the frequency after modulation, x be the pitch modulation value
+for a given time, and f be the base frequency.
+f' = (1 + x/32) * f    if x >= 0
+f' = (1 - x/256) * f   if x < 0")
+    (:pitch-freq :float "Frequency of the pitch modulation wave.")
+    (:pitch-top :int "Amplitude of the pitch modulation.")
+    (:pitch-offset :int "X offset of the pitch modulation wave.")
+    (:volume-model :wave-model "Wave model describing the volume modulation.")
+    (:volume-freq :float "Frequency of the volume modulation.")
+    (:volume-top :int "Amplitude [0-63] of the volume modulation.
+This form a fractional amount (out of 63) by which to modulate the volume.
+I.e. 63 could result in doubling/canceling out the audio and
+32 could result in volume increasing/decreasing by half.")
+    (:volume-offset :int "X offset of the volume modulation wave.")
+    (:initial-y :int "[0-63]. Initial value of base volume.
+These fields (ax-cy) create a volume envelope with linear interpolation
+between each point. This is calculated before the volume-model.")
+    (:ax :int "[0-255]. X value of when volume should be ay.")
+    (:ay :int "[0-63]. Value of x at time ax.")
+    (:bx :int "[ax-255].")
+    (:by :int "[0-63]")
+    (:cx :int "[bx-255]")
+    (:cy :int "[0-63]"))
+  "Ordered list of channel/track fields for a pxt file. 
+Each field is a (key parse-type description).")
+
+(defun read-pxt-file (path)
+  "Reads a pixtone (sound effect) file. Returns a list of tracks."
+  ;; The pxt file is a human readable : and newline separated form,
+  ;; followed by comma-separated values of all the same values.
+  ;; Because the csv are somewhat unreliable (might have extra curly-braces),
+  ;; I'll use the human readable values.
+
+  ;; Pxt files are 4 tracks. The tracks are only mixed if their :use
+  ;; flag is set, so discard any track with the use flag unset.
+  ;; The order of the tracks is irrelevant.
+  ;; The fields always come in the same order, so we do not check the
+  ;; contents of the field name.
+  (let* ((lines
+	 (split-sequence:split-sequence
+	  #\newline
+	  (remove #\return (read-file-into-string path))
+	  :remove-empty-subseqs t))
+	 (field-lines (subseq lines 0 (- (length lines) 4)))
+	 (fields (mapcar (comp second #_(split-sequence:split-sequence #\: _))
+			 field-lines))
+	 (num-fields (length *pxt-channel-fields*))
+	 (track1-fields (subseq fields 0 num-fields))
+	 (track2-fields (subseq fields num-fields (* 2 num-fields)))
+	 (track3-fields (subseq fields (* 2 num-fields) (* 3 num-fields)))
+	 (track4-fields (subseq fields (* 3 num-fields))))
+    (remove-if-not
+     (lambda (c) (aval c :use))
+     (list
+      (mapcar #'parse-pxt-channel-field track1-fields *pxt-channel-fields*)
+      (mapcar #'parse-pxt-channel-field track2-fields *pxt-channel-fields*)
+      (mapcar #'parse-pxt-channel-field track3-fields *pxt-channel-fields*)
+      (mapcar #'parse-pxt-channel-field track4-fields *pxt-channel-fields*)))))
+
+(defun parse-pxt-channel-field (val field)
+  (let ((type (second field))
+	(key (first field)))
+    (cons
+     key
+     (ecase type
+       (:int (parse-integer val))
+       (:float (read-from-string val))
+       (:bool (not (zerop (parse-integer val))))
+       (:wave-model (let ((idx (parse-integer val)))
+		      (first (assoc-value *pxt-wave-models* idx))))))))
+
+(defun pxt-tracks->wav (tracks)
+  "Converts tracks from a pxt file into an uncompressed .wav form
+for playback."
+  )
+
+(defun pxt-track->wav (track)
+  "Convert a single pxt track into an uncompressed .wav form."
+ )
+
+(defun pxt-track-function (track)
+  "Return a function that when given a byte offset returns
+the sound value."
+  (let* ((size (aval track :size))
+	 (pitch-wave (pitch-wave-function track))
+	 (volume-wave (volume-wave-function track)))
+    (lambda (x)
+      (let* ((pitch (funcall pitch-wave x))
+	     (main-freq (aval track :main-freq))
+	     (freq (cond ((< pitch 0) (* main-freq (+ 1 pitch)))
+			 (t (* main-freq (- 1 (/ pitch 8))))))
+	     (volume (funcall volume-wave x))
+	     (main-top (aval track :main-top))
+	     (top (+ main-top (* volume main-top)))
+	     (main-wave (pxt-wave-function size
+					   (aval track :main-model)
+					   freq
+					   top
+					   (aval track :main-offset))))
+	(/ (funcall main-wave x) 2)))))
+
+(defun pxt-wave-function (size model freq top offset)
+  "Generates a pixtone wave function f that calculates the
+sound at a given byte offset (between 0 and the size).
+Freq is the number of oscillations PER size (not per second.)
+Offset is a fractional amount out of 256 of a period to offset the wave."
+  (if (= 0 freq)
+      (constantly 0)
+      (let* ((amp (/ top 63))
+	     (period (/ size freq))
+	     (off (* period offset (/ 256))))
+	(lambda (x-amt)
+	  (let ((x (+ off x-amt)))
+	    (ecase model
+	      (:square
+	       (cond ((zerop (mod (floor x (/ period 2)) 2)) amp)
+		     (t (- amp))))
+	      (:sine
+	       (* amp (sin (* x 2 pi (/ period)))))
+	      (:saw-up
+	       (+ (- amp) (* 2 amp (/ period) (mod x period))))
+	      (:saw-down
+	       (+ amp (- (* 2 amp (/ period) (mod x period)))))
+	      (:triangle
+	       (let* ((p/4 (/ period 4))
+		      (quadrant (mod (floor x p/4) 4))
+		      (osc-start (* period (floor x period)))
+		      (slope (* 4 amp (/ period))))
+		 (cond ((or (= quadrant 1) (= quadrant 2))
+			(+ (* (- slope) (- x (+ osc-start p/4)))
+			   amp))
+		       ((= quadrant 0)
+			(+ (* slope (- x (- osc-start p/4)))
+			   (- amp)))
+		       (t
+			(+ (* slope (- x (+ osc-start (* 3 p/4))))
+			   (- amp))))))
+	      (:random
+	       (error "Random wave Not implemented"))))))))
+
+(defun plot-all-waves ()
+  (let* ((wave-function #_(pxt-wave-function 22050 _ 440.0 32 0))
+	 (x (map 'vector (lambda (x) x) (iota 1000)))
+	 (w (cl-plplot:basic-window)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function :triangle) x)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function :square) x)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function :sine) x)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function :saw-up) x)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function :saw-down) x)))
+    (cl-plplot:render w "xwin"))
+  (values))
+
+(defun plot-offsets ()
+  (let* ((wave-function #_(pxt-wave-function 22050 :saw-up 440.0 32 _))
+	 (x (map 'vector (lambda (x) x) (iota 1000)))
+	 (w (cl-plplot:basic-window)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function 0) x)))
+  
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector (funcall wave-function 128) x)))
+    (cl-plplot:render w "xwin"))
+  (values))
+
+(defun pitch-wave-function (track)
+  (pxt-wave-function (aval track :size)
+		     (aval track :pitch-model)
+		     (aval track :pitch-freq)
+		     (aval track :pitch-top)
+		     (aval track :pitch-offset)))
+(defun volume-wave-function (track)
+  (pxt-wave-function (aval track :size)
+		     (aval track :volume-model)
+		     (aval track :volume-freq)
+		     (aval track :volume-top)
+		     (aval track :volume-offset)))
+
+(defparameter *square-track*
+  '((:USE . T)
+    (:SIZE . 1000)
+    (:MAIN-MODEL . :square)
+    (:MAIN-FREQ . 44.0)
+    (:MAIN-TOP . 63)
+    (:MAIN-OFFSET . 0)
+    (:PITCH-MODEL . :square)
+    (:PITCH-FREQ . 1.0)
+    (:PITCH-TOP . 44)
+    (:PITCH-OFFSET . 0)
+    (:VOLUME-MODEL . :square)
+    (:VOLUME-FREQ . 1.0)
+    (:VOLUME-TOP . 23)
+    (:VOLUME-OFFSET . 0)
+    (:INITIAL-Y . 63)
+    (:AX . 0) (:AY . 63) (:BX . 164)
+    (:BY . 28) (:CX . 255) (:CY . 0)))
+
+(defun plot-track ()
+  (let* ((track
+	  *square-track*)
+	 (track-function (pxt-track-function track))
+	 (x (map 'vector #'identity (iota (aval *square-track* :size))))
+	 (w (cl-plplot:basic-window)))
+    (cl-plplot:add-plot-to-window
+     w
+     (cl-plplot:new-x-y-plot
+      x
+      (map 'vector track-function x)))
+    (cl-plplot:render w "xwin"))
+  (values))
+
+(plot-track)
